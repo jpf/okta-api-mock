1034b471fdfefcc8c207fc35804c1e4edddb85bb
Joel Franusic <joel.franusic@okta.com>
2015-11-11 13:35:34 -0800

    don't bind to 0.0.0.0

--- app.py
+++ app.py
@@ -1015,4 +1015,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     if port == 5000:
         app.debug = True
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
